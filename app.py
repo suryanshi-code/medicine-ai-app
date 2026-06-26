7daca71e5f7316e965b59b72f8c3ea3f6e1dc26e
YourName <your_email@gmail.com>
2026-06-26 20:52:22 +0530

    added patient names

--- app.py
+++ app.py
@@ -1,20 +1,83 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect
 
 app=Flask(__name__)
 
+patients=[]
+active_patient=0
+
 @app.route("/home",methods=["GET","POST"])
 def home():
-    name=""
-    age=""
-    symptoms=""
-    message=""
+    global active_patient
+
     if request.method=="POST":
+     
          name=request.form.get("patient","").upper()
          age=request.form.get("age","")
-         symptoms=request.form.get("symptoms","")
-         message= "Data recorded. Please consult a doctor."
+         symptoms=request.form.get("symptoms","").upper()
+         medicine=request.form.get("medicine","").upper()
+         dose=request.form.get("dose","")
+         time=request.form.get("time","").upper()
+        
+         if name and age and symptoms:
+              patient={
+                   "NAME": name, 
+                   "AGE":age,
+                   "SYMPTOMS":symptoms,
+                   "MEDICINE":medicine,
+                   "DOSE":dose,
+                   "TIME":time
 
-    return render_template("index.html",name=name,age=age,symptoms=symptoms,message=message)
+              }
+
+              patients.append(patient)
+              active_patient = len(patients) - 1
+
+                                             
+    if len(patients) == 0:
+         return render_template("index.html", patient=None,patients=[])
+    
+
+    # 🔥 HANDLE CLICK
+    clicked_name = request.args.get("name")
+
+    if clicked_name:
+        for i, p in enumerate(patients):
+            if p["NAME"] == clicked_name:
+                active_patient = i
+                selected_name = p
+                break
+    else:
+        selected_name = patients[active_patient]
+
+    return render_template("index.html",patients=patients,patient=selected_name)
+
+@app.route("/delete",methods=["POST"])
+def delete():
+     global active_patient
+
+     if len(patients) > 0:
+        patients.pop(active_patient)
+     return redirect("/home")
+
+@app.route("/add",methods=["POST"])
+def add():
+     global active_patient
+     patient={
+          "NAME": "", 
+          "AGE":"",
+          "SYMPTOMS":"",
+          "MEDICINE":"",
+          "DOSE":"",
+          "TIME":""
+
+     }
+          
+     patients.append(patient)
+
+     active_patient=len(patients)-1
+
+     return redirect("/home")
+
 
 if __name__ == "__main__":
      app.run(debug=True)
